3137f1ff7cbeef6b588d41e4b8aac3c7465fc8e2
Jacob <jtljac2@gmail.com>
2021-12-05 22:08:28 +0000

    Fixed setup steamcmd containing gmod setup script

--- Setup SteamCMD.bat
+++ Setup SteamCMD.bat
@@ -5,11 +5,3 @@ if not exist steamcmd/steamcmd.exe (
     powershell -Command "Expand-Archive -Force ./steamcmd.zip ./"
     cd ../
 )
-
-"./steamcmd/steamcmd.exe" +login anonymous +force_install_dir "../garrysmod" +app_update 4020 +quit
-"./steamcmd/steamcmd.exe" +login anonymous +force_install_dir "../cstrike" +app_update 232330 +quit
-
-
-if not exist garrysmod/cstrike/ mklink /J "./garrysmod/cstrike/" "./cstrike/cstrike/"
-
-pause
