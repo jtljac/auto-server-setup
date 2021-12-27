8c3accba96180aed8dfdc2ab6722f947d888aba3
Jacob Lucas <jtljac2@gmail.com>
2021-12-27 02:29:36 +0000

    Removed un-needed parts from the gmod run bat

--- garrysmod/run.bat
+++ garrysmod/run.bat
@@ -1,9 +1,3 @@
 @echo off
-if "%STEAMWEBAPIKEY"=="" (
-    echo "You can get your Steam Web API Key from https://steamcommunity.com/dev/apikey"
-    set /p apikey="Enter Steam WebAPI Key: "
-) else (
-    set apikey = "%STEAMWEBAPIKEY%"
-)
 
-start /wait srcds.exe -console -game garrysmod -tickrate 60 +map ttt_minecraft_b5 +maxplayers 16 +gamemode terrortown +host_workshop_collection 1354895497 -authkey %apikey%
+start /wait srcds.exe -console -game garrysmod -tickrate 60 +map ttt_minecraft_b5 +maxplayers 16 +gamemode terrortown +host_workshop_collection 1354895497
